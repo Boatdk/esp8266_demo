557244225e8adfaf3e7aac237574339cbaf3dd8a
supanut1911 <ynotmela@gmail.com>
2019-12-09 15:37:30 +0700

    inital commit

---
+++ esp8266Demo/Contorller/ControlVC.swift
@@ -0,0 +1,28 @@
+//
+//  ControlVC.swift
+//  esp8266Demo
+//
+//  Created by Supanut Laddayam on 9/12/2562 BE.
+//  Copyright © 2562 Supanut Laddayam. All rights reserved.
+//
+
+import UIKit
+
+class ControlVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+    
+    @IBAction func handleTapON(_ sender: Any) {
+        print("tap ON button")
+    }
+    
+    @IBAction func handleTapOFF(_ sender: Any) {
+        print("tap OFF buttn")
+    }
+    
+
+}
---
+++ esp8266Demo/Contorller/HomeVC.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  esp8266Demo
+//
+//  Created by Supanut Laddayam on 9/12/2562 BE.
+//  Copyright © 2562 Supanut Laddayam. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    
+    @IBOutlet weak var ipTextField: UITextField!
+    @IBOutlet weak var portTextField: UITextField!
+    @IBOutlet weak var connectBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    @IBAction func handleTabConnect(_ sender: Any) {
+        
+    }
+    
+    
+}
+
