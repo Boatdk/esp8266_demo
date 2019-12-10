e1aa1acd088811e1a24df885391d1dc8bcfeaf7e
Boatdk <boatz.dk@gmail.com>
2019-12-10 16:35:11 +0700

    add 2 button

--- esp8266Demo/Contorller/HomeVC.swift
+++ esp8266Demo/Contorller/HomeVC.swift
@@ -17,7 +17,7 @@ class HomeVC: UIViewController {
     //"switchs": "1","page": "1"
 
     @IBAction func handleTapPost(_ sender: Any) {
-        let parameter = ["switchs": "1", "page":"123456"]
+        let parameter = ["switch": "1", "page":"1"]
         
         guard let url = URL(string: "http://192.168.4.1/wifiinfo") else { return }
         var request = URLRequest(url: url)
@@ -43,7 +43,33 @@ class HomeVC: UIViewController {
         }.resume()
     }
     
-    
+    @IBAction func handleTapPost2(_ sender: Any) {
+        let parameter = ["switch": "1", "page":"2"]
+        
+        guard let url = URL(string: "http://192.168.4.1/wifiinfo") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameter, options: []) else {return}
+        request.httpBody = httpBody
+        
+        let session =  URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [] )
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+        }.resume()
+    }
+
     
     
 }
